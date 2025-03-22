8c86772f56fba25182da3e75880f509ab8088e62
Nikhil Ramchandani <ramchandaninikhil01@gmail.com>
2025-03-22 13:59:07 -0400

    fix

--- client/src/app/platform/providers.tsx
+++ client/src/app/platform/providers.tsx
@@ -1,34 +1,34 @@
 "use client";
 
 import { WagmiProvider, cookieToInitialState } from "wagmi";
-import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider, darkTheme, getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import { http, createStorage, cookieStorage } from 'wagmi';
-import { Chain, getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { Chain } from '@rainbow-me/rainbowkit';
 import { avalanche, avalancheFuji } from 'wagmi/chains';
 
-const projectId = "glacier-starter-kit";
-
-const supportedChains: Chain[] = [ avalanche, avalancheFuji ];
-
-export const config = getDefaultConfig({
-   appName: "Glacier Starter Kit",
-   projectId,
-   chains: supportedChains as any,
-   ssr: true,
-   storage: createStorage({
-    storage: cookieStorage,
-   }),
-  transports: supportedChains.reduce((obj, chain) => ({ ...obj, [chain.id]: http() }), {})
- });
+// Hardcoded WalletConnect project ID
+const projectId = "c9c5d37c2e1e03b0c14a4c0c4b4e8d4a";
 
 const queryClient = new QueryClient();
 
-type Props = {
+const supportedChains: Chain[] = [avalanche, avalancheFuji];
+
+export const config = getDefaultConfig({
+  appName: "Beantown Showdown",
+  projectId,
+  chains: supportedChains as any,
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  transports: supportedChains.reduce((obj, chain) => ({ ...obj, [chain.id]: http() }), {})
+});
+
+interface Props {
   children: React.ReactNode;
-  cookie?: string | null;
-};
+  cookie?: string;
+}
 
 export default function Providers({ children, cookie }: Props) {
   const initialState = cookieToInitialState(config, cookie);
