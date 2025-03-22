b798aee7fe90df82759afb86ad36f848a781b3c3
s-alad <n7.saad@gmail.com>
2025-03-22 01:15:46 -0400

    clean

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,23 +6,6 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex p-4"></div>
       <div className="mb-12 grid text-center lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
-          href="/balance-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            ERC-20 Token Balance{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Build a basic ERC-20 balance app
-          </p>
-        </a>
-
-        <a
           href="/basic-wallet"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
